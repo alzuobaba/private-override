7ebf6b6f4f7df9aba24413a08aa6cfbcbc8c6a89
alzuobaba <1292999356@qq.com>
2026-05-30 17:12:30 +0800

    feat: add debug logging for troubleshooting server error

--- scripts/apps/JS-1_PDFExpert.js
+++ scripts/apps/JS-1_PDFExpert.js
@@ -6,12 +6,27 @@
 *************************************/
 "use strict"
 
+console.log($script.name + ": PDF Expert script triggered");
+console.log($script.name + ": URL = " + $request.url);
+console.log($script.name + ": method = " + $request.method);
+
 var body = JSON.parse($response.body);
+
+console.log($script.name + ": original inAppStates count = " + (body.inAppStates ? body.inAppStates.length : 0));
+if (body.inAppStates && body.inAppStates.length > 0) {
+  console.log($script.name + ": original entitlements = " + JSON.stringify(body.inAppStates[0].entitlements));
+  console.log($script.name + ": original productId = " + body.inAppStates[0].productId);
+  console.log($script.name + ": original type = " + body.inAppStates[0].type);
+  console.log($script.name + ": original state = " + body.inAppStates[0].state);
+}
 
 body.inAppStates[0].entitlements = [
   "ios.pe.ai-features",
   "ios.pe.subscription.pdf-features"
 ];
 body.inAppStates[0].productId = "com.readdle.PDFExpert5.subscription.year50BMI_rollout";
+
+console.log($script.name + ": modified entitlements = " + JSON.stringify(body.inAppStates[0].entitlements));
+console.log($script.name + ": modified productId = " + body.inAppStates[0].productId);
 
 $done({ body: JSON.stringify(body) });
