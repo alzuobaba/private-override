e04bc7b392b2463ff2da5b0d0a339ef7054c677f
alzuobaba <1292999356@qq.com>
2026-05-30 17:07:56 +0800

    fix: replace inAppStates entirely with single PRO entry

--- scripts/apps/JS-1_PDFExpert.js
+++ scripts/apps/JS-1_PDFExpert.js
@@ -8,7 +8,7 @@
 
 var body = JSON.parse($response.body);
 
-body.inAppStates.push({
+body.inAppStates = [{
   "type": "subscription",
   "productId": "com.readdle.PDFExpert5.subscription.year50BMI_rollout",
   "originalTransactionId": 300001771036219,
@@ -25,24 +25,6 @@ body.inAppStates.push({
     "ios.pe.ai-features",
     "ios.pe.subscription.pdf-features"
   ]
-});
-
-body.inAppStates.push({
-  "type": "subscription",
-  "productId": "com.readdle.PDFExpert5.subscription.year50_pe6",
-  "originalTransactionId": 20000618444996,
-  "productName": "subscription",
-  "isEligibleForIntroPeriod": false,
-  "subscriptionExpirationDate": "13:15 03/11/2099",
-  "subscriptionExpirationTimestamp": 4097243700,
-  "subscriptionState": "active",
-  "subscriptionAutoRenewStatus": "autoRenewOn",
-  "isInGracePeriod": false,
-  "isInBillingRetryPeriod": false,
-  "entitlements": [
-    "ios.pe.ai-features",
-    "ios.pe.subscription.pdf-features"
-  ]
-});
+}];
 
 $done({ body: JSON.stringify(body) });
