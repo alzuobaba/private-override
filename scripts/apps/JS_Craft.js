c2b35fb1bcc672715addabaa41007900c7121726
alzuobaba <1292999356@qq.com>
2026-05-30 08:37:27 +0800

    fix: update Craft script for subscription model, fix all intervals to 86400

--- scripts/apps/JS_Craft.js
+++ scripts/apps/JS_Craft.js
@@ -1,75 +1,86 @@
-/*************************************
-
-项目名称：Craft-文件笔记编辑器解锁Premium
-下载地址：https://t.cn/A6HEVDeP
-更新日期：2024-10-15
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
-**************************************
-
-[rewrite_local]
-^https?:\/\/(api|docs)\.craft\.do\/(api\/)?(auth\/v\d\/profile|subscription\/(receipt|teams\/get-subscriptions)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Craft.js
-
-[mitm]
-hostname = api.craft.do
-
-*************************************/
 "use strict"
+
 console.log($script.name)
+console.log($script.name + ' Craft 订阅解锁')
 
+var url = $request.url
+var body = $response.body
 
+if (!body) { $done({}); }
 
+try {
+  var obj = JSON.parse(body)
+  console.log($script.name + ' ' + url)
 
-var chxm1023 = JSON.parse($response.body);
+  var future = 4092599349000
 
-if (/auth\/v\d\/profile/.test($request.url)) {
-  chxm1023.teams = chxm1023.teams || [{}];
-  chxm1023.spaces = chxm1023.spaces || [{}];
-  chxm1023.teams.forEach(team => {
-    team.tier = "V2_Team";
-    team.subscriptionInfo = {
-      "autoRenewStatus": true,
-      "provider": "AppStore",
-      "rawType": "AppStore",
-      "subscriptionId": "55555555-8888-1023-8888-999999999999",
-      "expirationTime": 4092599349000,
-      "renewPeriod": "Yearly"
-    };
-  });
-  chxm1023.spaces.forEach(space => {
-    space.tier = "V2_Team";
-  });
+  // Profile / Me endpoint - inject Pro subscription info
+  if (/profile|account|me/.test(url) && !/receipt|subscription/.test(url)) {
+    obj.tier = "V2_Team"
+    obj.subscriptionActive = true
+    obj.expirationTime = future
+    obj.autoRenewStatus = true
+    obj.provider = "AppStore"
+    obj.subscriptionType = "appStoreV2TeamYearly"
+
+    obj.teams = obj.teams || [{}]
+    obj.spaces = obj.spaces || [{}]
+    obj.teams.forEach(function (t) {
+      t.tier = "V2_Team"
+      t.subscriptionInfo = {
+        autoRenewStatus: true,
+        provider: "AppStore",
+        rawType: "AppStore",
+        subscriptionId: "55555555-8888-1023-8888-999999999999",
+        expirationTime: future,
+        renewPeriod: "Yearly"
+      }
+    })
+    obj.spaces.forEach(function (s) {
+      s.tier = "V2_Team"
+    })
+  }
+
+  // Subscription receipt endpoint
+  if (/receipt/.test(url)) {
+    obj.subscription = {
+      expirationDate: future,
+      subscriptionActive: true,
+      subscriptionId: "55555555-8888-1023-8888-999999999999",
+      productId: "com.lukilabs.lukiapp.v2_team_2.sub.yearly_test",
+      tier: "Pro",
+      subscriptionType: "appStoreV2TeamYearly",
+      rawSubscriptionType: "AppStore",
+      autoRenewStatus: true
+    }
+  }
+
+  // Teams / subscriptions list endpoint
+  if (/subscription.*team|team.*subscription|get-subscriptions/.test(url)) {
+    obj.subscriptions = obj.subscriptions || [{}]
+    obj.tier = "V2_Team"
+    obj.subscriptions.push({
+      isPrimary: true,
+      provider: "AppStore",
+      subscriptionId: "55555555-8888-1023-8888-999999999999",
+      renewPeriod: "Yearly",
+      rawType: "AppStore",
+      tier: "V2_Team",
+      expirationTime: future,
+      isActive: true
+    })
+  }
+
+  // Generic - add entitlement fields if they don't exist yet
+  if (!obj.tier && !obj.subscription) {
+    obj.subscriptionActive = true
+    obj.tier = "Pro"
+    obj.expirationDate = future
+    obj.autoRenewStatus = true
+  }
+
+  $done({ body: JSON.stringify(obj) })
+} catch (e) {
+  console.log($script.name + ' error: ' + e)
+  $done({})
 }
-
-
-if (/subscription\/receipt/.test($request.url)) {
-  chxm1023.subscription = {
-    "expirationDate": 4092599349000,
-    "subscriptionActive": true,
-    "subscriptionId": "55555555-8888-1023-8888-999999999999",
-    "productId": "com.lukilabs.lukiapp.v2_team_2.sub.yearly_test",
-    "tier": "Pro",
-    "subscriptionType": "appStoreV2TeamYearly",
-    "rawSubscriptionType": "AppStore",
-    "autoRenewStatus": true
-  };
-}
-
-if (/subscription\/teams\/get-subscriptions/.test($request.url)) {
-  chxm1023.subscriptions = chxm1023.subscriptions || [{}];
-  chxm1023.tier = "V2_Team";
-  chxm1023.subscriptions.push({
-    "isPrimary": true,
-    "provider": "AppStore",
-    "subscriptionId": "55555555-8888-1023-8888-999999999999",
-    "renewPeriod": "Yearly",
-    "rawType": "AppStore",
-    "tier": "V2_Team",
-    "expirationTime": 4092599349000,
-    "isActive": true
-  });
-}
-
-$done({ body: JSON.stringify(chxm1023) });
